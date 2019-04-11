f501235552a1bebb7990b05d0b4968717ae8e265
Junru1231 <junru1231@gmail.com>
2019-04-11 18:13:40 +0800

    Updates

---
+++ precache-manifest.1e175314461344d8ddfc43a802553db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d3b44484c2ce21e9247",
+    "url": "/company-order-form/static/css/main.b45d2900.chunk.css"
+  },
+  {
+    "revision": "4d3b44484c2ce21e9247",
+    "url": "/company-order-form/static/js/main.9e914fb2.chunk.js"
+  },
+  {
+    "revision": "c04ff68444adbcd34440",
+    "url": "/company-order-form/static/js/runtime~main.27a37cf0.js"
+  },
+  {
+    "revision": "8ecfcacb968a0eed5de6",
+    "url": "/company-order-form/static/css/2.65092a70.chunk.css"
+  },
+  {
+    "revision": "8ecfcacb968a0eed5de6",
+    "url": "/company-order-form/static/js/2.c1a59398.chunk.js"
+  },
+  {
+    "revision": "030fd00dd2a4e9d29dce5761901301c9",
+    "url": "/company-order-form/index.html"
+  }
+];
