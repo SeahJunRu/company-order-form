b7ce4865e63a9a4052c452e164d03c8eaf52900f
liwei123 <liwei13193@hotmail.com>
2019-04-14 20:09:06 +0800

    added no-cors to request header

--- src/Submission.js
+++ src/Submission.js
@@ -12,8 +12,8 @@ class Submission extends React.Component {
   	this.state = {
       showResult: false,
       schedule: [],
-      maxProfit: 0,
-      numOfComponents:[],
+      totalProfit: 0,
+      componentsLeft:[],
       isLoading: true,
       errors: null
     }
@@ -27,67 +27,67 @@ class Submission extends React.Component {
     const data = new FormData(event.target);
 	  const json = Object.assign(...Array.from(data, ([x,y]) => ({[x]:y})));
 	  const inputData = [{
-        "quickScan": this.state.rSelected,
-        "components": [{
-		  "name": "C1", 
-		  "cost": json.A1,
-		  "price": json.A2,
-		  "desiredUnit": json.A3,
-        }, {
-          "name": "C2",
-		  "cost": json.B1,
-		  "price": json.B2,
-          "desiredUnit": json.B3
-        }, {
-          "name": "C3",
-		  "cost": json.C1,
-		  "price": json.C2,
-          "desiredUnit": json.C3
-        }, {
-          "name": "C4",
-		  "cost": json.D1,
-		  "price": json.D2,
-          "desiredUnit": json.D3
-        }, {
-          "name": "C5",
-		  "cost": json.E1,
-		  "price": json.E2,
-          "desiredUnit": json.E3
-        }, {
-          "name": "C6",
-		  "cost": json.F1,
-		  "price": json.F2,
-          "desiredUnit": json.F3
-        }, {
-          "name": "C7",
-		  "cost": json.G1,
-		  "price": json.G2,
-          "desiredUnit": json.G3
-        }, {
-          "name": "C8",
-		  "cost": json.H1,
-		  "price": json.H2,
-          "desiredUnit": json.H3
-        } ]
-      }];
+      "quickScan": this.state.rSelected,
+      "components": [{
+  		  "name": "C1", 
+  		  "cost": json.A1,
+  		  "price": json.A2,
+  		  "desiredUnit": parseInt(json.A3, 10)
+      }, {
+        "name": "C2",
+	      "cost": json.B1,
+	      "price": json.B2,
+        "desiredUnit": parseInt(json.B3,10)
+      }, {
+        "name": "C3",
+	      "cost": json.C1,
+	      "price": json.C2,
+        "desiredUnit": parseInt(json.C3,10)
+      }, {
+        "name": "C4",
+	      "cost": json.D1,
+	      "price": json.D2,
+        "desiredUnit": parseInt(json.D3,10)
+      }, {
+        "name": "C5",
+	      "cost": json.E1,
+	      "price": json.E2,
+        "desiredUnit": parseInt(json.E3,10)
+      }, {
+        "name": "C6",
+	      "cost": json.F1,
+	      "price": json.F2,
+        "desiredUnit": parseInt(json.F3,10)
+      }, {
+        "name": "C7",
+	      "cost": json.G1,
+	      "price": json.G2,
+        "desiredUnit": parseInt(json.G3,10)
+      }, {
+        "name": "C8",
+	      "cost": json.H1,
+	      "price": json.H2,
+        "desiredUnit": parseInt(json.H3,10)
+      }]
+    }];
 	  alert(JSON.stringify(inputData));
     this.setState({ showResult: true })
 	  
-    axios.post(`https://a625a135-4311-42be-bdaa-2ab1ba840d67.mock.pstmn.io/getSchedule`, { json })
-      .then(response => {
-        console.log(response);
-        console.log(response.data);
-        this.setState({
-          schedule: response.data.schedule,
-          maxProfits: response.data.maxProfits,
-          numOfComponents: response.data.numOfComponents,
-          isLoading: false,
-          showResult: true
-        });
-      })
-      // If we catch any errors connecting, let's update accordingly
-      .catch(error => this.setState({ error, isLoading: false, showResult: false }));
-    
+    fetch('http://localhost:8080/solve', {
+      method: 'post',
+      body: inputData,
+      mode: 'no-cors'
+
+    }).then(function(response) {
+      this.setState({
+        schedule: response.data.schedule,
+        totalProfit: response.data.totalProfit,
+        componentsLeft: response.data.componentsLeft,
+        isLoading: false,
+        showResult: true
+      });
+    }).catch(error => this.setState({ error, isLoading: false, showResult: false }));
+
   }
   
   showHome() {
@@ -101,7 +101,7 @@ class Submission extends React.Component {
   }
   
   render() {
-    const { schedule, maxProfits, numOfComponents, isLoading, showResult } = this.state;  
+    const { schedule, totalProfit, componentsLeft, isLoading, showResult } = this.state;  
     if (showResult === true) {
       return (
         <div className='content-container'>  
@@ -113,12 +113,12 @@ class Submission extends React.Component {
               <React.Fragment>
               <div>
                 {!isLoading ? (
-                  // <h2> Maximum profit is {maxProfits} </h2> 
+                  // <h2> Total profit is {totalProfit} </h2> 
                   // display schedule
-                  numOfComponents.map(component => {
-                    const { name, number } = component;
+                  componentsLeft.map(component => {
+                    const { name, desiredUnit } = component;
                     return (
-                      <h3>Number of {name} : {number}</h3>
+                      <h3>Number of {name} not able to be produced: {desiredUnit}</h3>
                     );
                   })
                 ) : (
